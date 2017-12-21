e644a190f6af85d981aef51723b3a47d117b7a8a
Jonathan Grider <jagrider@purdue.edu>
2017-12-21 13:02:54 -0500

    Coding style, auto-layout update

--- Tipper-App/Tipper/SettingsViewController.swift
+++ Tipper-App/Tipper/SettingsViewController.swift
@@ -11,88 +11,76 @@ import UIKit
 let userDefaults = UserDefaults.standard
 
 class SettingsViewController: UIViewController {
-    
-    // Default tip components
-    @IBOutlet weak var defaultTipLabel: UILabel!
-    @IBOutlet weak var tipSegmentController: UISegmentedControl!
-    
-    // Dark Mode components
-    @IBOutlet weak var colorSwitch: UISwitch!
-    @IBOutlet weak var darkModeLabel: UILabel!
-    @IBOutlet weak var darkModeView: UIView!
-    @IBOutlet weak var settingsBar: UINavigationItem!
-    
-    /* parent items */
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // See if dark mode is enabled, update switch as needed
-        let isDark = userDefaults.bool(forKey: "darkMode")
-        isDark ? colorSwitch.setOn(true, animated: false) : colorSwitch.setOn(false, animated: false)
-        switchColorMode(self)
-        
-        // Update segment controller for default tips
-        tipSegmentController.selectedSegmentIndex = userDefaults.integer(forKey: "tipIndex")
-        tipSegmentController.isEnabled = true
-        tipSegmentController.tintColor = UIColor(red:0.00, green:0.48, blue:1.00, alpha:1.0)
-        
-        // Do any additional setup after loading the view.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  // Default tip components
+  @IBOutlet weak var defaultTipLabel: UILabel!
+  @IBOutlet weak var tipSegmentController: UISegmentedControl!
+  
+  // Dark Mode components
+  @IBOutlet weak var darkModeSwitch: UISwitch!
+  @IBOutlet weak var darkModeLabel: UILabel!
+  @IBOutlet weak var darkModeView: UIView!
+  @IBOutlet weak var settingsBar: UINavigationItem!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBAction func switchColorMode(_ sender: Any) {
-        
-        if colorSwitch.isOn {
-            
-            // Update userDefaults
-            userDefaults.set(true, forKey: "darkMode")
-            
-            // Update view components
-            self.view.backgroundColor = .black
-            view.tintColor = .black
-            darkModeView.backgroundColor = .black
-            darkModeLabel.textColor = .white
-            defaultTipLabel.textColor = .white
-            
-            // Update navigation area
-            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-            self.navigationController?.navigationBar.shadowImage = UIImage()
-            self.navigationController?.navigationBar.barTintColor = UIColor.black
-            self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
-            self.navigationController?.navigationBar.barStyle = .black
-        
-        } else {
-            
-            // Update userDefaults
-            userDefaults.set(false, forKey: "darkMode")
-            
-            // Update view components
-            self.view.backgroundColor = .white
-            view.tintColor = .white
-            darkModeView.backgroundColor = .white
-            darkModeLabel.textColor = .black
-            defaultTipLabel.textColor = .black
-            
-            // Update navigation area
-            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-            self.navigationController?.navigationBar.shadowImage = UIImage()
-            self.navigationController?.navigationBar.barTintColor = UIColor.white
-            self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.black]
-            self.navigationController?.navigationBar.barStyle = .default
-            
-            
-        }
+    // See if dark mode is enabled, update switch as needed
+    let isDark = userDefaults.bool(forKey: "darkMode")
+    isDark ? darkModeSwitch.setOn(true, animated: false) : darkModeSwitch.setOn(false, animated: false)
+    switchColorMode(self)
+    
+    // Update segment controller for default tips
+    tipSegmentController.selectedSegmentIndex = userDefaults.integer(forKey: "tipIndex")
+    tipSegmentController.isEnabled = true
+    tipSegmentController.tintColor = UIColor(red:0.00, green:0.48, blue:1.00, alpha:1.0)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  @IBAction func switchColorMode(_ sender: Any) {
+    if darkModeSwitch.isOn {
+      // Update userDefaults
+      userDefaults.set(true, forKey: "darkMode")
+      
+      // Update view components
+      view.backgroundColor = .black
+      view.tintColor = .black
+      darkModeView.backgroundColor = .black
+      darkModeLabel.textColor = .white
+      defaultTipLabel.textColor = .white
+      
+      // Update navigation area
+      navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+      navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+      navigationController?.navigationBar.shadowImage = UIImage()
+      navigationController?.navigationBar.barTintColor = .black
+      navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
+      navigationController?.navigationBar.barStyle = .black
+    } else {
+      
+      // Update userDefaults
+      userDefaults.set(false, forKey: "darkMode")
+      
+      // Update view components
+      view.backgroundColor = .white
+      view.tintColor = .white
+      darkModeView.backgroundColor = .white
+      darkModeLabel.textColor = .black
+      defaultTipLabel.textColor = .black
+      
+      // Update navigation area
+      navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+      navigationController?.navigationBar.shadowImage = UIImage()
+      navigationController?.navigationBar.barTintColor = .white
+      navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.black]
+      navigationController?.navigationBar.barStyle = .default
     }
-
-    @IBAction func updateDefaultTip(_ sender: Any) {
-        
-        userDefaults.set(tipSegmentController.selectedSegmentIndex, forKey: "tipIndex")
-        
-    }
+  }
+  
+  @IBAction func updateDefaultTip(_ sender: Any) {
+    userDefaults.set(tipSegmentController.selectedSegmentIndex, forKey: "tipIndex")
+  }
 }
--- Tipper-App/Tipper/ViewController.swift
+++ Tipper-App/Tipper/ViewController.swift
@@ -12,168 +12,150 @@ let defaults = UserDefaults.standard
 let tipPercentages = [0.15, 0.2, 0.25]
 
 class ViewController: UIViewController {
-
-    //@IBOutlet weak var billLabel: UILabel!
-    @IBOutlet weak var tipTextLabel: UILabel!
-    @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var totalTextLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipControl: UISegmentedControl!
-    @IBOutlet weak var tipTotalView: UIView!
+  
+  //@IBOutlet weak var billLabel: UILabel!
+  @IBOutlet weak var tipTextLabel: UILabel!
+  @IBOutlet weak var tipLabel: UILabel!
+  @IBOutlet weak var totalTextLabel: UILabel!
+  @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var billField: UITextField!
+  @IBOutlet weak var tipControl: UISegmentedControl!
+  @IBOutlet weak var tipInfoContainer: UIView!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+        
+    // Update default tips
+    updateToDefaultTips()
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // Update default tips
-        updateToDefaultTips()
-        
-        // Update the amount if within 10 minutes of last opening
-        let oldBill = defaults.double(forKey: "bill")
-        var oldTime = NSDate.init()
-        if (defaults.object(forKey: "time") != nil) {
-            oldTime = defaults.object(forKey: "time") as! NSDate
-            
-            if (oldTime.timeIntervalSinceNow <= 600 && oldBill != 0) {
-                billField.text = String(format: "%.2f", oldBill)
-            }
-        }
-        
-        // Set the tip & total view to be invisible
-        tipTotalView.alpha = 0
-        
-        // Update the colors
-        updateColors()
-        
-        // Bring up the keyboard right away
-        billField.keyboardType = UIKeyboardType.decimalPad
-        billField.becomeFirstResponder()
-        
+    // Update the amount if within 10 minutes of last opening
+    let oldBill = defaults.double(forKey: "bill")
+    var oldTime = NSDate.init()
+    if (defaults.object(forKey: "time") != nil) {
+      oldTime = defaults.object(forKey: "time") as! NSDate
+      
+      if (oldTime.timeIntervalSinceNow <= 600 && oldBill != 0) {
+        billField.text = String(format: "%.2f", oldBill)
+      }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        
-        // Update colors
-        updateColors()
-        
-        // Update tip controller & calculate tip
-        updateToDefaultTips()
-        calculateTip(self)
-        
-        // Bring up the keyboard right away
-        billField.keyboardType = UIKeyboardType.decimalPad
-        billField.becomeFirstResponder()
-    }
+    // Set the tip & total view to be invisible
+    tipInfoContainer.alpha = 0
     
-    override func viewWillDisappear(_ animated: Bool) {
-        
-        // Put keyboard away
-        view.endEditing(true)
-        
-        // Save time
-        let currTime = NSDate()
-        defaults.set(currTime, forKey: "time")
-        
-        // Save bill amount
-        defaults.set(Double(billField.text!) ?? 0, forKey: "bill")
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func onTap(_ sender: Any) {
-        // End keyboard editing
-        view.endEditing(true)
-    }
+    // Update the colors
+    updateColors()
     
-    @IBAction func calculateTip(_ sender: Any) {
-        
-        // Update the most recent bill value
-        defaults.set(Double(billField.text!) ?? 0, forKey: "bill")
-        
-        // Compute the tip and total
-        let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
-        
-        // Animate tip and total views
-        UIView.animate(withDuration: 0.4, animations:{
-            if (self.billField.text!.isEmpty) {
-                self.tipTotalView.alpha = 0
-            } else {
-                self.tipTotalView.alpha = 1
-            }
-        });
-        
-        // Update the tip and total labels
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        
+    // Bring up the keyboard right away
+    billField.keyboardType = UIKeyboardType.decimalPad
+    billField.becomeFirstResponder()
+    
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    
+    // Update colors, tip controller & calculate tip
+    updateColors()
+    updateToDefaultTips()
+    calculateTip(self)
+    
+    // Bring up the keyboard right away
+    billField.keyboardType = UIKeyboardType.decimalPad
+    billField.becomeFirstResponder()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    
+    // Put keyboard away
+    view.endEditing(true)
+    
+    // Save time
+    let currTime = NSDate()
+    defaults.set(currTime, forKey: "time")
+    
+    // Save bill amount
+    defaults.set(Double(billField.text!) ?? 0, forKey: "bill")
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  @IBAction func onTap(_ sender: Any) {
+    // End keyboard editing
+    view.endEditing(true)
+  }
+  
+  @IBAction func calculateTip(_ sender: Any) {
+    // Update the most recent bill value
+    defaults.set(Double(billField.text!) ?? 0, forKey: "bill")
+    
+    // Compute the tip and total
+    let billAmount = Double(billField.text!) ?? 0
+    let tipAmount = billAmount * tipPercentages[tipControl.selectedSegmentIndex]
+    let billTotal = billAmount + tipAmount
+    
+    // Animate tip and total views
+    UIView.animate(withDuration: 0.4, animations:{
+      if (self.billField.text!.isEmpty) {
+        self.tipInfoContainer.alpha = 0
+      } else {
+        self.tipInfoContainer.alpha = 1
+      }
+    });
+    
+    // Update the tip and total labels
+    tipLabel.text = String(format: "$%.2f", tipAmount)
+    totalLabel.text = String(format: "$%.2f", billTotal)
+  }
+  
+  func updateColors() {
+    let isDark = defaults.bool(forKey: "darkMode")
+    
+    if isDark {
+      /* update fields */
+      tipTextLabel.textColor = .white
+      totalTextLabel.textColor = .white
+      totalLabel.textColor = .white
+      billField.textColor = .white
+      billField.backgroundColor = .black
+      billField.tintColor = .lightGray
+      billField.textColor = .white
+      billField.keyboardAppearance = UIKeyboardAppearance.dark
+      tipInfoContainer.backgroundColor = .black
+      view.backgroundColor = .black
+      
+      /* update navbar */
+      navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+      navigationController?.navigationBar.shadowImage = UIImage()
+      navigationController?.navigationBar.barTintColor = .white
+      navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
+      navigationController?.navigationBar.barStyle = .black
+    } else {
+      /* update fields */
+      tipTextLabel.textColor = .black
+      totalTextLabel.textColor = .black
+      totalLabel.textColor = .black
+      billField.textColor = .black
+      billField.backgroundColor = .white
+      billField.textColor = .black
+      billField.keyboardAppearance = .light
+      tipInfoContainer.backgroundColor = .white
+      view.backgroundColor = .white
+      
+      /* update navbar */
+      navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+      navigationController?.navigationBar.shadowImage = UIImage()
+      navigationController?.navigationBar.barTintColor = .white
+      navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.black]
+      navigationController?.navigationBar.barStyle = .default
     }
-    
-    func updateColors() {
-        
-        let isDark = defaults.bool(forKey: "darkMode")
-        
-        if isDark {
-            
-            //tipLabel.textColor = .white
-            
-            tipTextLabel.textColor = .white
-            totalTextLabel.textColor = .white
-            totalLabel.textColor = .white
-            billField.textColor = .white
-            billField.backgroundColor = .black
-            billField.tintColor = .lightGray
-            billField.textColor = .white
-            billField.keyboardAppearance = UIKeyboardAppearance.dark
-            tipTotalView.backgroundColor = .black
-            self.view.backgroundColor = .black
-            
-            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-            self.navigationController?.navigationBar.shadowImage = UIImage()
-            self.navigationController?.navigationBar.barTintColor = UIColor.white
-            self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
-            self.navigationController?.navigationBar.barStyle = .black
-            
-        } else {
-            
-            //billLabel.textColor = .black
-            //tipLabel.textColor = .black
-            
-            tipTextLabel.textColor = .black
-            totalTextLabel.textColor = .black
-            totalLabel.textColor = .black
-            billField.textColor = .black
-            billField.backgroundColor = .white
-            billField.textColor = .black
-            billField.keyboardAppearance = UIKeyboardAppearance.light
-            tipTotalView.backgroundColor = .white
-            self.view.backgroundColor = .white
-            
-            /* update navbar */
-            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-            self.navigationController?.navigationBar.shadowImage = UIImage()
-            self.navigationController?.navigationBar.barTintColor = UIColor.white
-            self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.black]
-            self.navigationController?.navigationBar.barStyle = .default
-            
-        }
-        
-    }
-    
-    func updateToDefaultTips() {
-        
-        // Update the default tip selector
-        let tipIndex = defaults.integer(forKey: "tipIndex")
-        tipControl.selectedSegmentIndex = tipIndex
-    }
-    
-    
+  }
+  
+  func updateToDefaultTips() {
+    // Update the default tip selector
+    let tipIndex = defaults.integer(forKey: "tipIndex")
+    tipControl.selectedSegmentIndex = tipIndex
+  }
 }
-
